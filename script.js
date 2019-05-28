2be8c509c15254a24fb3a8e0f70b88486c99238e
Andreas saxvik <andreassaxvik@hotmail.com>
2019-05-28 11:04:22 +0200

    Setting up the sidebar with calender
    rough sidebar created and testing calender function

---
+++ script.js
@@ -0,0 +1,15 @@
+/*var element = document.getElementById("circlebtn", function(){
+   div.classList.toggle("active");
+}); */
+
+// funksjon to add class to an element to make it show. 
+// Its connected to circlebtn and boardwrapper.
+// var boardWrapper = document.getElementById("boardWrapper");
+
+// document.getElementById("circlebtn").addEventListener("click", function(){
+//    var itemDiv = document.createElement("div");
+//    itemDiv.classList.add("toggle-board");
+//    boardWrapper.appendChild(itemDiv);
+// });
+
+
