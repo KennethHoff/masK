e179503ae57cb6cf66a19dd041cb240eda1cb3a6
Andreas saxvik <andreassaxvik@hotmail.com>
2019-05-28 17:02:26 +0200

    fiddle with calender, ended up restructure in JS
    changed everything in HTML to JS, still testing and trying, not working properly

--- script.js
+++ script.js
@@ -11,5 +11,67 @@
 //    itemDiv.classList.add("toggle-board");
 //    boardWrapper.appendChild(itemDiv);
 // });
+(function(){
+   var ul = CreateUl("navigator", null, parentElement = document.getElementById("sidebar"));
 
+   memberList = [
+      {
+         name: "Filler"
+         // subMenu: []
+      },
+      {
+         name: "Member",
+         subMenu: ["Kenneth", "asd", "asf", "basd"]
+      },
+      {
+         name: "Filler"
+         // submenu: []
+      }];
 
+   // document.getElementById("sidebar").appendChild(ul);
+   RenderMemberList();
+
+   function RenderMemberList() {
+      RenderArray(memberList, ul);
+    }
+   function RenderArray(array, parentElement) {
+      for (var i = 0; i < array.length; i++){
+         RenderObject(array[i], ul);
+      }
+   }
+
+   function RenderObject(objEle, ulElement){
+      var li = createLi(objEle);
+      ulElement.appendChild(li);
+
+      if (typeof(objEle) === String || objEle.subMenu === undefined || objEle.subMenu.length === 0) return;
+      console.log(objEle.name + " has submenu");
+      var subMenuUL = CreateUl(null, null, li);
+      RenderArray(objEle.subMenu, subMenuUL);
+   }
+})();
+
+function createLi(objEle) {
+   var li = document.createElement("li");
+   li.setAttribute("class", "no");
+   console.log("Name ? " + objEle.name);
+   if (typeof(objEle) == String) {
+      console.log("hallo?");
+      li.innerHTML += objEle;
+      return li;
+   }
+
+   if (Array.isArray(objEle.subMenu) && objEle.subMenu.length >= 0 ) li.setAttribute("class", "haveSubMenu");
+
+   li.innerHTML += objEle.name;
+      
+   return li;
+}
+
+function CreateUl(id, cl, parentElement) {
+   var ul = document.createElement("ul");
+   if (id != undefined || id != null ) ul.setAttribute("id", id);
+   if (cl != undefined || cl != null) ul.setAttribute("class", cl);
+   if (parentElement != undefined || parentElement != null) parentElement.appendChild(ul);
+   return ul;
+}
