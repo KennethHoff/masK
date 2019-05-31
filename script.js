ca33d9c4856dc582f2dc51d0dadd88d82a24a93f
Andreas saxvik <andreassaxvik@hotmail.com>
2019-05-31 21:08:41 +0200

    Removed somethings that was not need and fixed comments and some styling
    So added better comments and did some small styling changes, also removed the whole calender function,

--- script.js
+++ script.js
@@ -1,79 +1,150 @@
-(function(){
-   var ul = CreateUl("navigator", null, parentElement = document.getElementById("sidebar"));
+// CurrentIndex is the variable that ID generator uses to generate IDs
+// Or numbers from 0 to length of array.
+var currentIndex = 0;
 
+/*--------------------FUNCTIONS----------------------*/
+/** 
+ * Main function that runs the other functions inside it
+ * Its also here that the ulNavigator = ul with id "navigator"
+ * Sidebar is where everything is inside.
+*/
+(function(){
+   var ulNavigator = CreateUl("navigator", null, document.getElementById("sidebar"));
+/*---------------ARRAY---------------*/
+
+/**
+ * MemberList array with navigation buttons and (members) with a potential calender function. 
+*/
    memberList = [
       {
-         name: "Filler"
+         name: "Filler",
+         html: "<a href='#'> Filler </a>"
          // subMenu: []
       },
       {
-         name: "Member",
-         subMenu: ["Kenneth", "asd", "asf", "basd"],
-         subMenu2: ["cal", "cal", "cal", "cal"]
+         name: "Members",
+         subMenu: [
+            {
+               name: "Andreas",
+               subMenu: ["Kenneth Calendar?"]
+            },
+            {
+               name: "Kristoffer",
+               subMenu: ["Kristian Calendar?"]
+            },
+            {
+               name: "Kenneth",
+               subMenu: ["Kenneth Calendar?"]
+            },
+            {
+               name: "Marcin",
+               subMenu: ["Kenneth Calendar?"]
+            },
+            {
+               name: "Kristian",
+               subMenu: ["Kenneth Calendar?"]
+            },
+         ]
       },
       {
-         name: "Filler"
+         name: "Filler",
+         html: "<a href='#'> Filler </a>"
          // subMenu: []
       },
       {
-         name: "Filler"
+         name: "Filler",
+         html: "<a href='#'> Filler </a>"
          // subMenu: []
       },
       {
-         name: "Filler"
+         name: "Filler",
+         html: "<a href='#'> Filler </a>"
          // submenu: []
       }];
+// ------------------- ARRAY end --------------------
 
-   // document.getElementById("sidebar").appendChild(ul);
+// --------------------- Render function ------------------
    RenderMemberList();
 
+/** 
+ * this function calls the RenderArray function with the memberList.
+ * 
+ * */
    function RenderMemberList() {
-      RenderArray(memberList, ul);
+      RenderArray(memberList, ulNavigator);
     }
-   function RenderArray(array) {
+    /** 
+     * This function iterates through the array given, and if the ul is null, set the ul to ulNavigator
+     * @param {string[]} array a given array
+     * @param {string} ul a given ul element, if the ul is null. Set it to ulNavigator.
+     * */
+   function RenderArray(array, ul) {
       for (var i = 0; i < array.length; i++){
-         RenderObject(array[i], ul);
+         RenderObject(array[i], (ul === null ? ulNavigator : ul));
       }
    }
-/*A function that render an object, in this case objEle = li and ulElement
-also saves the function createLi(objEle) which creates an Li to the variable Li
-and adds or create the Li under ul element navigator.  */
-   function RenderObject(objEle, ulElement){
-      var li = createLi(objEle);
+/**  
+ * A function that render an object, in this case objEle = li and ulElement
+ * also saves the function createLi(objEle) which creates an Li to the variable Li
+ * and create the Li appended to the given ulElement.  
+ * @param {String} objEle is the object element in the array aka [ { Element is inside } <-- obj ]
+ * @param {String} ulElement is an ul element which an li is appended to.
+ *  
+*/
+   function RenderObject(objEle, ulElement) {
+      var li = CreateLi(objEle = objEle);
       ulElement.appendChild(li);
 
-/* Checks if the type of objEle === string or .subMenu === undefined or objEle.subMenu.length === 0 if it is return 0 or undefined or string
-signs the CreateUl(null, null, li) function to the variable subMenuUl
-calls on the RenderArray function and loops through the objEle, subMenu, subMenuUl to create it.
+/* 
+   Checks if the type of objEle === string or .subMenu === undefined or objEle.subMenu.length === 0 if it is return
+   signs the CreateUl(null, null, li) function to the variable subMenuUl
+   calls on the RenderArray function with the array parameter: 'subMenu' in the objEle object, 
+   and ul parameter: subMenuUl, which is a newly created ul. aka dropdown.
 */
-      if (typeof(objEle) === String || objEle.subMenu === undefined || objEle.subMenu.length === 0) return;
-      console.log(objEle.name + " have sub menu");
-      var subMenuUL = CreateUl(null, null, li);
+      if (typeof(objEle) === "string" || objEle.subMenu === undefined || objEle.subMenu.length === 0) return;
+      li.setAttribute("class", "subMenu hasSubMenu");
+      var subMenuUL = CreateUl(null, "dropdown", li);
       RenderArray(objEle.subMenu, subMenuUL);
    }
 })();
-/*Function named createLi that creates an li and sets the attribute class="" to the li that is made */
-function createLi(objEle) {
+// ------------------- Render function end ------------------------
+
+
+/*-----------------FUNCTION FOR LI AND UL ------------------*/
+
+/**
+ * Function named createLi that creates an li and sets the attribute id="" to the li that is made 
+ * @param {string} objEle is the object element in the array aka [ { Element is inside } <-- obj ]
+ * @param {Element} li HTML element that is created.
+ * @returns {Element} HTML element li.  
+ * */
+function CreateLi(objEle) {
    var li = document.createElement("li");
-   li.setAttribute("class", "test");
-   console.log("Name ? " + objEle.name);
-   if (typeof(objEle) == String) {
-      console.log("hallo?");
-      li.innerHTML += objEle;
+   li.setAttribute("id", IDGenerator());
+   if (typeof(objEle) === "string") {
+      li.innerHTML = objEle;
       return li;
    }
-/* checks if objEle.subMenu is an array and that the objEle.subMenu.length is greater than or equal to 0
-set the attribute class="haveSubMenu". objEle is the li. */
-   if (Array.isArray(objEle.subMenu) && objEle.subMenu.length >= 0 ) li.setAttribute("class", "haveSubMenu");
+   
+/** 
+ * checks if objEle.subMenu is an array and that the objEle.subMenu.length is greater than or equal to 0
+   set the attribute class="hasSubMenu". objEle is the li.
+*/
+   if (Array.isArray(objEle.subMenu) && objEle.subMenu.length >= 0 ) li.setAttribute("class", "hasSubMenu");
 /*get the String value inside the li pluss the li name or objEle.name*/
-   li.innerHTML += objEle.name;
+   li.innerHTML = (objEle.html !== undefined ? objEle.html : objEle.name);
       /*returns the li */
    return li;
 }
-/*This function creates an Ul and sets an id and or class if its not undefined or not null
-also check if parentElement = sidebar is not undefined or not null, if its not then add or create
-ul under parentElement. 
-*/
+/** 
+ * This function creates an Ul and sets an id and or class if its not undefined or not null
+   also check if parentElement is not undefined or not null, if its not then create
+   a new ul appended to parentElement.
+
+ * @param {String} id id of the ul if ul is not undefined or null.
+ * @param {string} cl Class of the ul if ul is not undefined or null
+ * @param {object} parentElement is a parant of the ul
+ */
 function CreateUl(id, cl, parentElement) {
    var ul = document.createElement("ul");
    if (id != undefined || id != null ) ul.setAttribute("id", id);
@@ -81,20 +152,18 @@ function CreateUl(id, cl, parentElement) {
    if (parentElement != undefined || parentElement != null) parentElement.appendChild(ul);
    return ul;
 }
+/*-----------------FUNCTION FOR LI AND UL end ------------------*/
 
-var nav = document.getElementById("navigator");
+/*---------------ID GENERATOR----------------------*/
 
-for(var ul of memberList){
-   var li = document.createElement("li");
-   li.classList.add("hasSub")
-   nav.appendChild(li);
-
-   var navi = document.createElement("ul");
-   li.appendChild(navi);
-
-   for(var secoundUl of memberList){
-      var secoundLi = document.createElement("li");
-      secoundLi.classList.add("subMenu");
-      navi.appendChild(secoundLi);
-   }
+/**
+ * Returns the current value of currentIndex, and subsequently increment the value
+ * @param {id} IDGenerator a function that generates ID
+ * @return {string} return currentIndex returns the current value of currentIndex.
+ *  
+ */
+function IDGenerator(){
+   return currentIndex++;
 }
+
+//---------------- ID GENERATOR end -----------------
