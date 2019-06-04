499e42b909bda8429b656598c9130973a851324a
amish1188 <martin.joodd@gmail.com>
2019-06-04 16:59:07 +0200

    did some changes with columns and buttons

---
+++ Desktop/Website/Website/JS/login.js
@@ -0,0 +1,22 @@
+$(loginMenuBtn).on("click", loginMenu);
+$(signUpMenuBtn).on("click", signUpMenu);
+
+$(loginBtn).on("click", login)
+
+function signUpMenu() {
+    document.getElementById("login").style.display ="none";
+    document.getElementById("signup").style.display="block";
+    document.getElementById("signUp").classList.add("active");
+    document.getElementById("logIn").classList.remove("active");
+}
+
+function loginMenu() {
+    document.getElementById("signup").style.display="none";
+    document.getElementById("login").style.display="block";
+    document.getElementById("logIn").classList.add("active");
+    document.getElementById("signUp").classList.remove("active");
+}
+
+function login() {
+    location.href = "../HTML/incubator.html";
+}
